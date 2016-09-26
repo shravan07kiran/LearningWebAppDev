1da65ce7d770bffd7a75129e33e60ecad4df193a
shravan07kiran <shravan07kiran@csu.fullerton.edu>
2016-09-26 01:20:30 -0700

    Finished Assignment2(example2)

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,21 +1,41 @@
 var main = function () {
     "use strict";
-
+	var $;
     var addCommentFromInputBox = function () {
         var $new_comment;
+		var $u2_comment;
 
         if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+		$new_comment = $("<p class=\"u1\">").text($(".comment-input input").val());
             $new_comment.hide();
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
+		else if ($(".user2comment-input input").val() !== "") {
+            $u2_comment = $("<p class=\"u2\">").text($(".user2comment-input input").val());
+            $u2_comment.hide();
+            $(".comments").append($u2_comment);
+            $u2_comment.fadeIn();
+            $(".user2comment-input input").val("");
+        }
+		
     };
 
-    $(".comment-input button").on("click", function (event) {
+    $(".user2comment-input button").on("click", function (event1) {
         addCommentFromInputBox();
     });
+	
+
+    $(".user2comment-input input").on("keypress", function (event1) {
+        if (event1.keyCode === 13) {
+            addCommentFromInputBox();
+        }
+    });
+	$(".comment-input button").on("click", function (event) {
+        addCommentFromInputBox();
+    });
+	
 
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
